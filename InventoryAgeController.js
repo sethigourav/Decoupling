029e59f7ff025a4ff40888d313dae342b15bc092
Gourav sethi <gourav.sethi@outlook.com>
2019-11-14 13:16:00 +0530

    Modifications for InventoryAge files

--- InventoryAgeController.js
+++ InventoryAgeController.js
@@ -1,20 +1,103 @@
-﻿topazApp.controller('InventoryAgeController', ['$scope', '$server', function ($scope, $server) {
+﻿topazApp.controller('InventoryAgeController', ['$scope', '$server','$http', function ($scope, $server, $http) {
 
     $scope.checkCostDataHidden();
 
     $scope.on("selectionBoxesLoaded", function () {
-        $server.bindData("OrganizationInventoryAgeCount", $scope, "organizationCountAgeData", { callback: function (d) { $scope.organizationUrls = d.urls } });
-        $server.bindData("OrganizationInventoryAgeCost", $scope, "organizationCostAgeData", { callback: function (d) { $scope.organizationUrls = d.urls } });
-        $server.bindData("OrganizationInventoryAgePercentCount", $scope, "organizationPercentCountAgeData", { callback: function (d) { $scope.organizationUrls = d.urls } });
-        $server.bindData("OrganizationInventoryAgePercentCost", $scope, "organizationPercentCostAgeData", { callback: function (d) { $scope.organizationUrls = d.urls } });
-        $server.bindData("LocationInventoryAgeCount", $scope, "locationCountAgeData", { callback: function (d) { $scope.locationUrls = d.urls } });
-        $server.bindData("LocationInventoryAgeCost", $scope, "locationCostAgeData", { callback: function (d) { $scope.locationUrls = d.urls } });
-        $server.bindData("LocationInventoryAgePercentCount", $scope, "locationPercentCountAgeData", { callback: function (d) { $scope.locationUrls = d.urls } });
-        $server.bindData("LocationInventoryAgePercentCost", $scope, "locationPercentCostAgeData", { callback: function (d) { $scope.locationUrls = d.urls } });
+        $scope.applyFilterButtonEnabled = true
     });
-
+   
     $scope.rawData.url = "InventoryAgeRawData";
+    $scope.locationSelectionBox.visible = false;
     
+    $scope.goFilter = {
 
+        submit: function () {
+
+            $scope.submitValues();
+
+            if ($scope.urlParameters.Organization || $scope.urlParameters.Location || $scope.urlParameters.Employee || $scope.urlParameters.AssetID || $scope.urlParameters.SerialNo || $scope.urlParameters.Manufacturer || $scope.urlParameters.ModelNo || $scope.urlParameters.ModelDesc || $scope.urlParameters.CapitalInput) {
+                $scope.applyFilterClicked = true;
+
+                var config = {
+                    headers: {
+                        'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
+                    }
+                }
+
+                var querystring = window.location.search.substring(1);
+               
+                $http.post("/API/OrganizationInventoryAgeCount" + '?hash_id=' + Math.random(), querystring, config)
+                    .success(function (data) {
+                  
+                        data = typeof data === 'string' && data && (data[0] == '{' || data[0] == '[' || data == "true" || data == "false") ? eval('(' + data + ')') : data;
+                        $scope.organizationUrls = data.urls;
+                        $scope.organizationCountAgeData = data;
+            
+                    });
+               
+                $http.post("/API/OrganizationInventoryAgeCost" + '?hash_id=' + Math.random(), querystring, config)
+                    .success(function (data) {
+           
+                        data = typeof data === 'string' && data && (data[0] == '{' || data[0] == '[' || data == "true" || data == "false") ? eval('(' + data + ')') : data;
+                        $scope.organizationUrls = data.urls;
+                        $scope.organizationCostAgeData = data;                        
+      
+                    });
+
+                $http.post("/API/OrganizationInventoryAgePercentCount" + '?hash_id=' + Math.random(), querystring, config)
+                    .success(function (data) {
+           
+                        data = typeof data === 'string' && data && (data[0] == '{' || data[0] == '[' || data == "true" || data == "false") ? eval('(' + data + ')') : data;
+                        $scope.organizationUrls = data.urls;
+                        $scope.organizationPercentCountAgeData = data;
+                    });
+
+                    
+                $http.post("/API/OrganizationInventoryAgePercentCost" + '?hash_id=' + Math.random(), querystring, config)
+                .success(function (data) {
+       
+                    data = typeof data === 'string' && data && (data[0] == '{' || data[0] == '[' || data == "true" || data == "false") ? eval('(' + data + ')') : data;
+                    $scope.organizationUrls = data.urls;
+                    $scope.organizationPercentCostAgeData = data;
+                });
+
+                $http.post("/API/LocationInventoryAgeCount" + '?hash_id=' + Math.random(), querystring, config)
+                .success(function (data) {
+       
+                    data = typeof data === 'string' && data && (data[0] == '{' || data[0] == '[' || data == "true" || data == "false") ? eval('(' + data + ')') : data;
+                    $scope.organizationUrls = data.urls;
+                    $scope.locationCountAgeData = data;
+                });
+              
+                $http.post("/API/LocationInventoryAgeCost" + '?hash_id=' + Math.random(), querystring, config)
+                .success(function (data) {
+       
+                    data = typeof data === 'string' && data && (data[0] == '{' || data[0] == '[' || data == "true" || data == "false") ? eval('(' + data + ')') : data;
+                    $scope.locationUrls = data.urls;
+                    $scope.locationCostAgeData = data;
+                });
+
+                $http.post("/API/LocationInventoryAgePercentCount" + '?hash_id=' + Math.random(), querystring, config)
+                .success(function (data) {
+       
+                    data = typeof data === 'string' && data && (data[0] == '{' || data[0] == '[' || data == "true" || data == "false") ? eval('(' + data + ')') : data;
+                    $scope.locationUrls = data.urls;
+                    $scope.locationPercentCountAgeData = data;
+                });
+
+                $http.post("/API/LocationInventoryAgePercentCost" + '?hash_id=' + Math.random(), querystring, config)
+                .success(function (data) {
+       
+                    data = typeof data === 'string' && data && (data[0] == '{' || data[0] == '[' || data == "true" || data == "false") ? eval('(' + data + ')') : data;
+                    $scope.locationUrls = data.urls;
+                    $scope.locationPercentCostAgeData = data;
+                });
+
+            }
+            else {
+                $scope.applyFilterClicked = false;
+            }
+        }
+    };
 
 }]);
