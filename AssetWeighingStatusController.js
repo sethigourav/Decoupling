d57fb21ec5a27b4776a26beebac4ac5e3238068d
Gourav sethi <gourav.sethi@outlook.com>
2019-11-21 20:40:06 +0530

    Initial commit for the new files for decoupling

---
+++ AssetWeighingStatusController.js
@@ -0,0 +1,20 @@
+﻿topazApp.controller('AssetWeighingStatusController', ['$scope', '$server', function ($scope, $server) {
+    $scope.on("selectionBoxesLoaded", function () {
+        $server.bindData("OrganizationWeighedUnweighedAssetCounts", $scope, "organizationWeighedUnweighedAssetCounts", { callback: function (d) { $scope.organizationUrls = d.urls }});
+        $server.bindData("LocationWeighedUnweighedAssetCounts", $scope, "locationWeighedUnweighedAssetCounts", { callback: function (d) { $scope.locationUrls = d.urls } });
+        $server.bindData("WeighedUnweighedAssetTable", $scope, "weighedUnweighedAssetTable");
+        $server.bindData("WeighedUnweighedModelTable", $scope, "weighedUnweighedModelTable");
+    });
+
+    $scope.calculateTotal = function (key, totals) {
+        if (key == 'unweighed' || key == 'weighed') {
+            return totals[key];
+        } else {
+            return totals['weighed'] / (parseFloat(totals['weighed']) + parseFloat(totals['unweighed']));
+        }
+    };
+
+    $scope.employeeSelectionBox.visible = true;
+
+    $scope.rawData.url = "WeighedUnweighedAssetRawData";
+}]);
