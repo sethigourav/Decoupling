5b88074b0fde171e7c5ca3a86cdcb482562d277e
Gourav sethi <gourav.sethi@outlook.com>
2019-11-21 21:26:28 +0530

    Modifications for Assestweight component

--- AssetWeighingStatusController.js
+++ AssetWeighingStatusController.js
@@ -1,10 +1,57 @@
-﻿topazApp.controller('AssetWeighingStatusController', ['$scope', '$server', function ($scope, $server) {
+﻿topazApp.controller('AssetWeighingStatusController', ['$scope', '$server', '$http',function ($scope, $server, $http) {
+    
     $scope.on("selectionBoxesLoaded", function () {
-        $server.bindData("OrganizationWeighedUnweighedAssetCounts", $scope, "organizationWeighedUnweighedAssetCounts", { callback: function (d) { $scope.organizationUrls = d.urls }});
-        $server.bindData("LocationWeighedUnweighedAssetCounts", $scope, "locationWeighedUnweighedAssetCounts", { callback: function (d) { $scope.locationUrls = d.urls } });
-        $server.bindData("WeighedUnweighedAssetTable", $scope, "weighedUnweighedAssetTable");
-        $server.bindData("WeighedUnweighedModelTable", $scope, "weighedUnweighedModelTable");
+        $scope.applyFilterButtonEnabled = true;        
     });
+
+    $scope.goFilter = {
+
+        submit: function () {
+            $scope.applyFilterClicked = true;
+
+                var config = {
+                    headers: {
+                        'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
+                    }
+                }
+
+                var querystring = window.location.search.substring(1);
+
+                $http.post("/API/OrganizationWeighedUnweighedAssetCounts" + '?hash_id=' + Math.random(), querystring, config)
+                .success(function (data) {
+
+                    data = typeof data === 'string' && data && (data[0] == '{' || data[0] == '[' || data == "true" || data == "false") ? eval('(' + data + ')') : data;
+
+                    $scope.organizationWeighedUnweighedAssetCounts = data;   
+                    $scope.organizationUrls = data.urls                                  
+                });
+
+                $http.post("/API/LocationWeighedUnweighedAssetCounts" + '?hash_id=' + Math.random(), querystring, config)
+                .success(function (data) {
+
+                    data = typeof data === 'string' && data && (data[0] == '{' || data[0] == '[' || data == "true" || data == "false") ? eval('(' + data + ')') : data;
+
+                    $scope.locationWeighedUnweighedAssetCounts = data;   
+                    $scope.organizationUrls = data.urls                                  
+                });
+
+                $http.post("/API/WeighedUnweighedAssetTable" + '?hash_id=' + Math.random(), querystring, config)
+                .success(function (data) {
+
+                    data = typeof data === 'string' && data && (data[0] == '{' || data[0] == '[' || data == "true" || data == "false") ? eval('(' + data + ')') : data;
+
+                    $scope.weighedUnweighedAssetTable = data;                                                  
+                });
+
+                $http.post("/API/WeighedUnweighedModelTable" + '?hash_id=' + Math.random(), querystring, config)
+                .success(function (data) {
+
+                    data = typeof data === 'string' && data && (data[0] == '{' || data[0] == '[' || data == "true" || data == "false") ? eval('(' + data + ')') : data;
+
+                    $scope.weighedUnweighedModelTable = data;                                                  
+                });
+            }
+    };
 
     $scope.calculateTotal = function (key, totals) {
         if (key == 'unweighed' || key == 'weighed') {
